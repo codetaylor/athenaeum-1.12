4b34caf28f5bed0ff84d05ead50575e5d4a4ae2b
Jason Taylor <jason@codetaylor.com>
2018-03-04 20:06:43 -0800

    FEATURE: client render vars

---
+++ src/main/java/com/codetaylor/mc/athenaeum/animation/vars/FloatVar.java
@@ -0,0 +1,21 @@
+package com.codetaylor.mc.athenaeum.animation.vars;
+
+public class FloatVar {
+
+  private float value;
+
+  public FloatVar(float value) {
+
+    this.value = value;
+  }
+
+  public float getValue() {
+
+    return this.value;
+  }
+
+  public void setValue(float value) {
+
+    this.value = value;
+  }
+}
---
+++ src/main/java/com/codetaylor/mc/athenaeum/animation/vars/IntVar.java
@@ -0,0 +1,21 @@
+package com.codetaylor.mc.athenaeum.animation.vars;
+
+public class IntVar {
+
+  private int value;
+
+  public IntVar(int value) {
+
+    this.value = value;
+  }
+
+  public int getValue() {
+
+    return this.value;
+  }
+
+  public void setValue(int value) {
+
+    this.value = value;
+  }
+}
