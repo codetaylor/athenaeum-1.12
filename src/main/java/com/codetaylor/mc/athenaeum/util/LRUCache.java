5de187374a54c9da9f0cfb6d71607b30aab80f22
Jason Taylor <jason@codetaylor.com>
2018-03-25 00:50:54 -0700

    Add LRU cache

---
+++ src/main/java/com/codetaylor/mc/athenaeum/util/LRUCache.java
@@ -0,0 +1,32 @@
+package com.codetaylor.mc.athenaeum.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<K, V>
+    extends LinkedHashMap<K, V> {
+
+  public static <K, V> LRUCache<K, V> newInstance(int capacity) {
+
+    return new LRUCache<>(capacity);
+  }
+
+  private int maxSize;
+
+  public LRUCache(int capacity) {
+
+    super(capacity, 0.75f, true);
+    this.maxSize = capacity;
+  }
+
+  @Override
+  protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+
+    return this.size() > this.maxSize;
+  }
+
+  private LRUCache() {
+    //
+  }
+
+}
