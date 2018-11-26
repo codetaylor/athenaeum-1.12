e60a72fcecac8611acad445c440ac1ad2f89257b
Jason Taylor <jason@codetaylor.com>
2018-11-26 08:29:07 -0800

    Add array helper methods

--- src/main/java/com/codetaylor/mc/athenaeum/util/ArrayHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/util/ArrayHelper.java
@@ -1,5 +1,7 @@
 package com.codetaylor.mc.athenaeum.util;
 
+import java.util.Arrays;
+
 public final class ArrayHelper {
 
   public static String[] copy(String[] toCopy) {
@@ -14,6 +16,31 @@ public final class ArrayHelper {
     int[] result = new int[toCopy.length];
     System.arraycopy(toCopy, 0, result, 0, toCopy.length);
     return result;
+  }
+
+  public static <T> boolean contains(T[] array, T element) {
+
+    return Arrays.asList(array).contains(element);
+  }
+
+  public static int getOrLast(int[] array, int index) {
+
+    if (index >= array.length) {
+      return array[array.length - 1];
+
+    } else {
+      return array[index];
+    }
+  }
+
+  public static <T> T getOrLast(T[] array, int index) {
+
+    if (index >= array.length) {
+      return array[array.length - 1];
+
+    } else {
+      return array[index];
+    }
   }
 
   private ArrayHelper() {
