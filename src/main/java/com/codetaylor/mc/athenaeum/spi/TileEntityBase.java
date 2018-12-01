3bba5e6c05fff6aaa549939f16bb72c320a5e5e8
Jason Taylor <jason@codetaylor.com>
2018-11-30 16:34:20 -0800

    Add common override of shouldRefresh to tile base

--- src/main/java/com/codetaylor/mc/athenaeum/spi/TileEntityBase.java
+++ src/main/java/com/codetaylor/mc/athenaeum/spi/TileEntityBase.java
@@ -2,6 +2,10 @@ package com.codetaylor.mc.athenaeum.spi;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public class TileEntityBase
     extends TileEntity {
@@ -14,6 +18,21 @@ public class TileEntityBase
       IBlockState blockState = this.world.getBlockState(this.getPos());
       this.world.notifyBlockUpdate(this.getPos(), blockState, blockState, 3);
     }
+  }
+
+  @Override
+  public boolean shouldRefresh(
+      World world,
+      BlockPos pos,
+      @Nonnull IBlockState oldState,
+      @Nonnull IBlockState newState
+  ) {
+
+    if (oldState.getBlock() == newState.getBlock()) {
+      return false;
+    }
+
+    return super.shouldRefresh(world, pos, oldState, newState);
   }
 
 }
