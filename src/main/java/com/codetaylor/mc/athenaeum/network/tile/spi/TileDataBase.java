ac3774da303d7098423ce3d1e97ee516cf1ec568
Jason Taylor <jason@codetaylor.com>
2018-12-06 11:58:53 -0800

    Add observers for tile data dirty state

--- src/main/java/com/codetaylor/mc/athenaeum/network/tile/spi/TileDataBase.java
+++ src/main/java/com/codetaylor/mc/athenaeum/network/tile/spi/TileDataBase.java
@@ -1,5 +1,10 @@
 package com.codetaylor.mc.athenaeum.network.tile.spi;
 
+import net.minecraft.tileentity.TileEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This is the base tile data element.
  * <p>
@@ -9,20 +14,65 @@ package com.codetaylor.mc.athenaeum.network.tile.spi;
 public abstract class TileDataBase
     implements ITileData {
 
+  public interface IChangeObserver<D extends TileDataBase> {
+
+    void onDirtyStateChanged(D data);
+
+    class OnDirtyMarkTileDirty<D extends TileDataBase>
+        implements IChangeObserver<D> {
+
+      private final TileEntity tile;
+
+      public OnDirtyMarkTileDirty(TileEntity tile) {
+
+        this.tile = tile;
+      }
+
+      @Override
+      public void onDirtyStateChanged(D data) {
+
+        if (data.isDirty()) {
+          this.tile.markDirty();
+        }
+      }
+    }
+  }
+
   private final int updateInterval;
   private int updateCounter;
   private boolean dirty;
   private boolean forceUpdate;
+  private List<IChangeObserver> changeObservers;
 
   protected TileDataBase(int updateInterval) {
 
     this.updateInterval = updateInterval;
   }
 
+  public void addChangeObserver(IChangeObserver observer) {
+
+    if (this.changeObservers == null) {
+      this.changeObservers = new ArrayList<>(1);
+    }
+
+    this.changeObservers.add(observer);
+  }
+
   @Override
   public void setDirty(boolean dirty) {
 
+    boolean changed = (this.dirty != dirty);
+
     this.dirty = dirty;
+
+    if (this.changeObservers != null
+        && changed) {
+      //noinspection ForLoopReplaceableByForEach
+      for (int i = 0; i < this.changeObservers.size(); i++) {
+        //noinspection unchecked
+        this.changeObservers.get(i).onDirtyStateChanged(this);
+      }
+    }
   }
 
   @Override
