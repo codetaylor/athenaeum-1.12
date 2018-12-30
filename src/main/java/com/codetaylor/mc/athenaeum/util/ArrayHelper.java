c25a81473dd1b2b724d4b6ab477d614d0eb9be40
Jason Taylor <jason@codetaylor.com>
2018-12-30 11:38:33 -0800

    Add array combine util

--- src/main/java/com/codetaylor/mc/athenaeum/util/ArrayHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/util/ArrayHelper.java
@@ -1,8 +1,52 @@
 package com.codetaylor.mc.athenaeum.util;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public final class ArrayHelper {
+
+  /**
+   * Combines two arrays of the same or most general type and returns a new array.
+   * <p>
+   * https://stackoverflow.com/a/80503
+   *
+   * @param a
+   * @param b
+   * @param <E>
+   * @return
+   */
+  public static <E> E combine(E a, E b) {
+
+    if (!a.getClass().isArray() || !b.getClass().isArray()) {
+      throw new IllegalArgumentException();
+    }
+
+    Class<?> resCompType;
+    Class<?> aCompType = a.getClass().getComponentType();
+    Class<?> bCompType = b.getClass().getComponentType();
+
+    if (aCompType.isAssignableFrom(bCompType)) {
+      resCompType = aCompType;
+
+    } else if (bCompType.isAssignableFrom(aCompType)) {
+      resCompType = bCompType;
+
+    } else {
+      throw new IllegalArgumentException();
+    }
+
+    int aLen = Array.getLength(a);
+    int bLen = Array.getLength(b);
+
+    @SuppressWarnings("unchecked")
+    E result = (E) Array.newInstance(resCompType, aLen + bLen);
+    //noinspection SuspiciousSystemArraycopy
+    System.arraycopy(a, 0, result, 0, aLen);
+    //noinspection SuspiciousSystemArraycopy
+    System.arraycopy(b, 0, result, aLen, bLen);
+
+    return result;
+  }
 
   public static String[] copy(String[] toCopy) {
 
