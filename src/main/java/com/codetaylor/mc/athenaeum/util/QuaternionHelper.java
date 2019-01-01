191e291f7b8cd89f2fe20e205c041af78376b8c2
Jason Taylor <jason@codetaylor.com>
2019-01-01 13:19:56 -0800

    Add quaternion mult helper

--- src/main/java/com/codetaylor/mc/athenaeum/util/QuaternionHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/util/QuaternionHelper.java
@@ -30,6 +30,31 @@ public final class QuaternionHelper {
     return q;
   }
 
+  /**
+   * Multiplies q0 by q1. The result is returned in res. It should be noted that
+   * quaternion multiplication is not commutative so q * p != p * q.
+   * <p>
+   * It is safe if q0 == res || q1 == res.
+   *
+   * @param q0  the quaternion to multiply
+   * @param q1  the quaternion to multiply
+   * @param res the quaternion to store the result in.
+   * @return the new quaternion.
+   */
+  public static Quaternion mult(Quaternion q0, Quaternion q1, Quaternion res) {
+
+    if (res == null) {
+      res = new Quaternion();
+    }
+    float q0w = q0.w, q0x = q0.x, q0y = q0.y, q0z = q0.z;
+    float q1w = q1.w, q1x = q1.x, q1y = q1.y, q1z = q1.z;
+    res.x = q0x * q1w + q0y * q1z - q0z * q1y + q0w * q1x;
+    res.y = -q0x * q1z + q0y * q1w + q0z * q1x + q0w * q1y;
+    res.z = q0x * q1y - q0y * q1x + q0z * q1w + q0w * q1z;
+    res.w = -q0x * q1x - q0y * q1y - q0z * q1z + q0w * q1w;
+    return res;
+  }
+
   private QuaternionHelper() {
     //
   }
