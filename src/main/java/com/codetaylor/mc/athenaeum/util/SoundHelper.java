9be03c9161840d1b018df1bf67fa256dd49a23b3
Jason Taylor <jason@codetaylor.com>
2019-01-15 18:55:36 -0800

    Add sound helper

---
+++ src/main/java/com/codetaylor/mc/athenaeum/util/SoundHelper.java
@@ -0,0 +1,32 @@
+package com.codetaylor.mc.athenaeum.util;
+
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public final class SoundHelper {
+
+  public static void playSoundServer(World world, BlockPos pos, SoundEvent soundEvent, SoundCategory soundCategory) {
+
+    if (world.isRemote) {
+      return;
+    }
+
+    world.playSound(
+        null,
+        pos.getX() + 0.5f,
+        pos.getY() + 0.5f,
+        pos.getZ() + 0.5f,
+        soundEvent,
+        soundCategory,
+        0.5F + (float) Math.random() * 0.5F,
+        0.9F + (float) Math.random() * 0.15F
+    );
+  }
+
+  private SoundHelper() {
+    //
+  }
+
+}
