1582d443653484aa7c2302b4d2aaede484a276e6
Jason Taylor <jason@codetaylor.com>
2019-01-21 15:25:41 -0800

    Add AABB helper method to create AABB's

--- src/main/java/com/codetaylor/mc/athenaeum/util/AABBHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/util/AABBHelper.java
@@ -4,6 +4,11 @@ import net.minecraft.util.math.AxisAlignedBB;
 
 public final class AABBHelper {
 
+  public static AxisAlignedBB create(double x0, double y0, double z0, double x1, double y1, double z1) {
+
+    return new AxisAlignedBB(x0 / 16.0, y0 / 16.0, z0 / 16.0, x1 / 16.0, y1 / 16.0, z1 / 16.0);
+  }
+
   public static boolean contains(AxisAlignedBB bounds, double x, double y, double z) {
 
     if (x > bounds.minX && x < bounds.maxX) {
