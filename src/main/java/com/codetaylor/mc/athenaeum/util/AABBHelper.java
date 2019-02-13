56c19ebaa2f8b64679745f441f21cbfd3c7f0ec5
Jason Taylor <jason@codetaylor.com>
2019-02-12 17:11:55 -0800

    Add aabb rotation method

--- src/main/java/com/codetaylor/mc/athenaeum/util/AABBHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/util/AABBHelper.java
@@ -4,6 +4,21 @@ import net.minecraft.util.math.AxisAlignedBB;
 
 public final class AABBHelper {
 
+  private static final double SIN_90 = Math.sin(Math.PI / 2.0);
+  private static final double COS_90 = Math.cos(Math.PI / 2.0);
+
+  public static AxisAlignedBB rotateHorizontalCCW90Centered(AxisAlignedBB axisAlignedBB) {
+
+    return new AxisAlignedBB(
+        0.5 + (axisAlignedBB.minX - 0.5) * COS_90 - (axisAlignedBB.minZ - 0.5) * SIN_90,
+        axisAlignedBB.minY,
+        0.5 + (axisAlignedBB.minX - 0.5) * SIN_90 + (axisAlignedBB.minZ - 0.5) * COS_90,
+        0.5 + (axisAlignedBB.maxX - 0.5) * COS_90 - (axisAlignedBB.maxZ - 0.5) * SIN_90,
+        axisAlignedBB.maxY,
+        0.5 + (axisAlignedBB.maxX - 0.5) * SIN_90 + (axisAlignedBB.maxZ - 0.5) * COS_90
+    );
+  }
+
   public static AxisAlignedBB create(double x0, double y0, double z0, double x1, double y1, double z1) {
 
     return new AxisAlignedBB(x0 / 16.0, y0 / 16.0, z0 / 16.0, x1 / 16.0, y1 / 16.0, z1 / 16.0);
