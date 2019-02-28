104e62e6bf0ccb5ce69307218e960ea7f3f09afb
Jason Taylor <jason@codetaylor.com>
2019-02-27 18:26:50 -0800

    Add tooltip helper method

--- src/main/java/com/codetaylor/mc/athenaeum/util/TooltipHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/util/TooltipHelper.java
@@ -4,6 +4,8 @@ import com.codetaylor.mc.athenaeum.ModAthenaeum;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
 
+import java.util.List;
+
 public class TooltipHelper {
 
   public static final String TOOLTIP_COMMON_HOLD_SHIFT = "gui." + ModAthenaeum.MOD_ID + ".tooltip.common.hold_shift";
@@ -15,5 +17,15 @@ public class TooltipHelper {
         TextFormatting.AQUA,
         TextFormatting.DARK_GRAY
     );
+  }
+
+  public static void addTooltip(List<String> tooltip, String text, int preferredIndex) {
+
+    if (tooltip.size() > preferredIndex) {
+      tooltip.add(preferredIndex, text);
+
+    } else {
+      tooltip.add(text);
+    }
   }
 }
