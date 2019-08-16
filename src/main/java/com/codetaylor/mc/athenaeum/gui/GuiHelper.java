622aef5c6064f7f514f8e0d4d216d2201ff0d10d
Jason Taylor <jason@codetaylor.com>
2019-08-16 09:39:08 -0700

    Add gui helper method

--- src/main/java/com/codetaylor/mc/athenaeum/gui/GuiHelper.java
+++ src/main/java/com/codetaylor/mc/athenaeum/gui/GuiHelper.java
@@ -16,6 +16,20 @@ import java.awt.*;
 
 public class GuiHelper {
 
+  public static void drawModalRectWithCustomSizedTexture(int x, int y, int z, float u, float v, int width, int height, float textureWidth, float textureHeight) {
+
+    float f = 1.0F / textureWidth;
+    float f1 = 1.0F / textureHeight;
+    Tessellator tessellator = Tessellator.getInstance();
+    BufferBuilder bufferbuilder = tessellator.getBuffer();
+    bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+    bufferbuilder.pos((double) x, (double) (y + height), z).tex((double) (u * f), (double) ((v + (float) height) * f1)).endVertex();
+    bufferbuilder.pos((double) (x + width), (double) (y + height), z).tex((double) ((u + (float) width) * f), (double) ((v + (float) height) * f1)).endVertex();
+    bufferbuilder.pos((double) (x + width), (double) y, z).tex((double) ((u + (float) width) * f), (double) (v * f1)).endVertex();
+    bufferbuilder.pos((double) x, (double) y, z).tex((double) (u * f), (double) (v * f1)).endVertex();
+    tessellator.draw();
+  }
+
   public static void drawStringOutlined(
       String translateKey,
       int x,
